d77fc3db60843592736ef94fb5d8fefe7b827711
kotturi rahul <kotturirahul1996@gmail.com>
2022-02-09 10:38:40 -0600

    initial commit

---
+++ Lesson1/public/controller/firebase-auth.js
@@ -0,0 +1,11 @@
+import * as Elements from '../viewpage/elements.js'
+
+export function addEventListeners() {
+
+}
+
+Elements.formSignIn.addEventListener('submit', e => {
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+
+});
