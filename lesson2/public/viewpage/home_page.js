14ae670902e09b7ce814c2c7ee03d0adaea7d88c
kotturi rahul <kotturirahul1996@gmail.com>
2022-02-19 00:07:03 -0600

    final commit

--- lesson2/public/viewpage/home_page.js
+++ lesson2/public/viewpage/home_page.js
@@ -1,11 +1,12 @@
 import * as Elements from './elements.js'
 import { routePathnames } from '../controller/route.js';
 import { currentUser } from '../controller/firebase_auth.js';
-import {Product} from '../model/product.js'
+import { Product } from '../model/product.js'
 import * as CloudFunctions from '../controller/cloud_functions.js'
 import * as Util from './util.js'
 import * as Constants from '../model/constants.js'
 import * as Cloudstorage from '../controller/cloud_storage.js'
+import * as EditProduct from '../controller/edit_product.js'
 
 let imageFile2upload = null;
 
@@ -65,6 +66,28 @@ export async function home_page() {
     });
 
     Elements.root.innerHTML = html;
+
+    const forms = document.getElementsByClassName('form-edit-delete-product')
+    for (let i = 0; i < forms.length; i++) {
+        forms[i].addEventListener('submit', async e => {
+             e.preventDefault();
+             const buttons = e.target.getElementsByTagName('button');
+             const submitter = e.target.submitter;
+             if (submitter == 'EDIT') {
+                 const label = Util.disableButton(buttons[0]);
+                 await EditProduct.edit_product(e.target.docId.value);
+                 await Util.sleep(1000)
+                 Util.enableButton(buttons[0], label);
+             } else if (submitter == 'DELETE') {
+                 const label = Util.disableButton(buttons[1]);
+                 await EditProduct.delete_product(e.target.docId.value, e.target.imageName.value);
+                 await Util.sleep(1000)
+                 Util.enableButton(buttons[1], label);
+             } else {
+                 console.log('No such submitter', submitter);
+             }
+        })
+    }
 }
 
 async function addNewProduct(e) {
@@ -73,13 +96,13 @@ async function addNewProduct(e) {
     const price = e.target.price.value;
     const summary = e.target.summary.value;
 
-    const product = new Product({name, price, summary});
+    const product = new Product({ name, price, summary });
 
-    const  button = e.target.getElementsByTagName('button')[0];
+    const button = e.target.getElementsByTagName('button')[0];
     const label = Util.disableButton(button);
 
     try {
-        const {imageName, imageURL} = await Cloudstorage.uploadImage(imageFile2upload);
+        const { imageName, imageURL } = await Cloudstorage.uploadImage(imageFile2upload);
         product.imageName = imageName;
         product.imageURL = imageURL;
         const docId = await CloudFunctions.addProduct(product.toFirestore());
@@ -97,11 +120,19 @@ async function addNewProduct(e) {
 
 function buildProductCard(product) {
     return `
-    <div class="card d-inline-flex" style="width: 18rem;">
+    <div id="card-${product.docId}" class="card d-inline-flex" style="width: 18rem;">
     <img src="${product.imageURL}" class="card-img-top">
         <div class="card-body">
             <h5 class="card-title">${product.name}</h5>
             <p class="card-text">$ ${product.price.toFixed(2)}<br>${product.summary}</p>
+            <form class="form-edit-delete-product" method="post">
+            <input type="hidden" name="docId" value="${product.docId}">
+            <input type="hidden" name="imageName" value="${product.imageName}">
+            <button type="submit" class="btn btn-outline-primary"
+              onclick="this.form.submitter='EDIT'">Edit</button>
+            <button type="submit" class="btn btn-outline-danger"
+              onclick="this.form.submitter='DELETE'">Delete</button>
+            </form>
         </div>
     </div>
     `;
